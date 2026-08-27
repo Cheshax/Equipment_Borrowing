618d93270bdfdc95ea5816efafc3a78cc1955994
Cajetas <beverlynicolecajetas8@gmail.com>
2026-08-28 02:47:02 +0800

    git add . git commit -m "Add Domain Models"

---
+++ src/EquipmentBorrowing.Domain/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace EquipmentBorrowing.Domain;
+
+public class Student
+{
+    public int Id { get; }
+    public string name { get; }
+    public bool IsAllowedtoBorrow { get; private set; }
+
+    public Student(int id, string name, bool isAllowedtoBorrow = true)
+    {
+        Id = id;
+        this.name = name;
+        IsAllowedtoBorrow = isAllowedtoBorrow;
+    }
+
+    public void Suspend() => IsAllowedtoBorrow = false;
+    public void Reinstate() => IsAllowedtoBorrow = true;
+
+}
